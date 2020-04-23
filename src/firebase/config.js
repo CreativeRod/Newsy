479205af28c41a519b0b3a5220b44d505f22d4e8
Creative Roderick <CreativeRoderick@gmail.com>
2020-04-23 14:28:30 -0500

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDyW6DDy0cvNrJXkCFFV988sn67qpYnMHo",
+  authDomain: "newsy-9c0b7.firebaseapp.com",
+  databaseURL: "https://newsy-9c0b7.firebaseio.com",
+  projectId: "newsy-9c0b7",
+  storageBucket: "newsy-9c0b7.appspot.com",
+  messagingSenderId: "814151740024",
+  appId: "1:814151740024:web:b1009036c74e75b40c9b8d"
+};
+
+export default firebaseConfig;
