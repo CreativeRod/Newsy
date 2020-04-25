e84193f4f140627b96336866afb5863a20b64025
Creative Roderick <CreativeRoderick@gmail.com>
2020-04-24 21:53:44 -0500

    useForm Hook and Validators

--- src/pages/Auth/Forgot.js
+++ src/pages/Auth/Forgot.js
@@ -1,20 +1,46 @@
 import React from "react";
-import { IonPage, IonInput, IonContent, IonItem, IonLabel, IonRow, IonCol, IonButton } from "@ionic/react";
+import { IonPage, IonInput, IonContent, IonItem, IonLabel, IonRow, IonCol, IonButton, IonLoading } from "@ionic/react";
 import NavHeader from "../../components/Header/NavHeader";
+import validatePasswordReset from "../../validators/validatePasswordReset";
+import useForm from "../../hooks/useForm";
+import firebase from "../../firebase";
+import {toast} from "../../helpers/toast";
 
-const Forgot = () => {
+const INITIAL_STATE = {
+  email: "",
+}
+
+const Forgot = (props) => {
+  const {handleSubmit, handleChange, values, isSubmitting} = useForm(INITIAL_STATE, validatePasswordReset, handleResetPassword);
+  const [busy, setBusy] = React.useState(false);
+
+  async function handleResetPassword() {
+    setBusy(true);
+    const { email } = values;
+    try {
+      await firebase.resetPassword(email);
+      toast("Check your email to reset your password.");
+      props.history.push("/login");
+    } catch (err) {
+      console.error("Password Reset Error", err);
+      toast(err.message);
+    }
+    setBusy(false);
+  }
+
   return (
     <IonPage>
       <NavHeader title="Password Reset" />
+      <IonLoading message={"Please wait..."} isOpen={busy} />
       <IonContent>
         <IonItem lines="full">
           <IonLabel position="floating">Email</IonLabel>
-          <IonInput name="email" type="text" required>
+          <IonInput name="email" type="text" value={values.email} onIonChange={handleChange} required>
           </IonInput>
         </IonItem>
         <IonRow>
           <IonCol>
-            <IonButton type="submit" color="primary" expand="block">
+            <IonButton type="submit" color="primary" expand="block" onClick={handleSubmit} disabled={isSubmitting}>
               Get Rest Link
             </IonButton>
           </IonCol>
